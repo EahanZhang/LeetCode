96fcabfe1254761857fbaf0f300506ab4a6572ef
EahanZhang <yhzhang211@gmail.com>
2016-12-07 21:37:26 +0800

    9. Palindrome Number

---
+++ PalindromeNumber.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+		if (x < 0 || x != 0 && x % 10 == 0) {
+			return false;
+		}
+		int rel = 0;
+		while (x > rel) {
+			rel = rel * 10 + x % 10;
+			x = x / 10;
+		}
+        return (x == rel || x == rel / 10);
+    }
+}
